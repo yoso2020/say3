f770237989d81fea7e1873509515fb976310a355
yoso2020 <yoso2020@foxmail.com>
2022-03-31 21:27:41 +0800

    finished board

--- src/app/components/checkerboard/checkerboard.component.ts
+++ src/app/components/checkerboard/checkerboard.component.ts
@@ -25,4 +25,17 @@ export class CheckerboardComponent implements OnInit, OnChanges {
     this.rotatedLength = this.boardWidth - this.basicWidth * 2;
     this.rotatedOffset = Math.sqrt(this.rotatedLength * this.rotatedLength + this.rotatedLength * this.rotatedLength) / 2;
   }
+
+  getStateText() {
+    const player = this.battleService.currentPlayer
+    if (player.doMarking) {
+      return player.name + '执行删除标志'
+    } else if (player.doDeleting) {
+      return player.name + '删除对方棋子'
+    } else if (player.doMoving) {
+      return player.name + '移动棋子'
+    } else {
+      return this.battleService.state === 'Started' ? player.name + '行动' : player.name + '落子'
+    }
+  }
 }
