a7a2942800fcb1a825163df62ba8c4f28fae6d6a
Yosong <yoso2020@foxmail.com>
2022-04-01 14:08:06 +0000

    !2 publish first version * Merge branch 'master' of gitee.com:yoso2020/say3 into enhance-player-state * debounce * first version

--- src/app/components/piece/piece.component.ts
+++ src/app/components/piece/piece.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core'
+import {Component, Input, OnChanges} from '@angular/core'
 import {Piece} from '../../shared/models/piece'
 import {BattleService} from '../../shared/services/battle.service'
 
@@ -7,21 +7,22 @@ import {BattleService} from '../../shared/services/battle.service'
   templateUrl: './piece.component.html',
   styleUrls: ['./piece.component.scss']
 })
-export class PieceComponent implements OnInit {
+export class PieceComponent implements OnChanges {
   @Input() piece: Piece
   @Input() basicWidth: number
   pieceStyle = {}
   constructor(public battleService: BattleService) {
   }
 
-  ngOnInit(): void {
-    // piece length is 0.25 basic length
+  ngOnChanges(): void {
+    // piece length is 0.5 basic length
     // piece offset is 50% piece length, so offset = index + 1 - 0.5 / 2 = 0.75
+    // border width is 3px
     const pieceLength = this.basicWidth / 2
     const pieceLengthStyle = pieceLength + 'px'
     this.pieceStyle = {
-      left: (this.piece.colIndex + 0.75) * this.basicWidth + 'px',
-      top: (this.piece.rowIndex + 0.75) * this.basicWidth + 'px',
+      left: (this.piece.colIndex + 0.75)  * this.basicWidth + (this.piece.colIndex+1) * 2 + 'px',
+      top: (this.piece.rowIndex + 0.75) * this.basicWidth + (this.piece.rowIndex +1) * 2 + 'px',
       width: pieceLengthStyle,
       height: pieceLengthStyle
     }
